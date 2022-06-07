68fadcf204b4351d8b455e719c2f63c7a8a92393
denisdelaki <denisdelaki@gmail.com>
2022-06-07 21:55:21 +0300

    commit tree 14c034e9db3ba51cc47e3a6287444d34653e4ff7

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(message = "roller-skate") {
+  return `This Saturday, I want to ${message}!`;
+}
+saturdayFun();
+function mondayWork(messo = "go to the office") {
+  return `This Monday, I will ${messo}.`;
+}
+function wrapAdjective(result='*') {
+    return function (emphatic = "a hard worker") {
+        return `You are ${result }${emphatic}${result}!`
+    };
+    
+}
